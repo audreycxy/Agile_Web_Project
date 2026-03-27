17c05bc57048b717dccdc7fdd01a58429d8f24c2
Sitong Liu <24244417@student.uwa.edu.au>
2026-03-27 15:30:28 +0800

    Add initial home page files

---
+++ Clicking_Game/static/js/home.js
@@ -0,0 +1,11 @@
+document.getElementById("loginBtn").onclick = function () {
+  alert("Go to login page");
+};
+
+document.getElementById("signupBtn").onclick = function () {
+  alert("Go to sign up page");
+};
+
+document.getElementById("guestBtn").onclick = function () {
+  alert("Continue as guest");
+};
