68ab5395b6089df84e382d45fe170665a2aa8418
James-909 <j.w.osmond@outlook.com>
2026-05-08 18:25:25 +0800

    Added: loading overlay, egg animation and jinja to html. Improved game state structure.

--- Clicking_Game/routes/main.py
+++ Clicking_Game/routes/main.py
@@ -1,5 +1,6 @@
 # Handles public routes that don't require authentication 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
+from Clicking_Game.models import users
 
 bp = Blueprint("main", __name__)
 
@@ -13,4 +14,24 @@ def guest():
 
 @bp.route("/game")
 def game():
-    return render_template("game.html")
+    initial_state = {
+        "points": 0,
+        "current_level": 1,
+        "current_type": "standard",
+        "clicks_remaining": None,
+        "progress_percent": 0,
+        "is_guest": True
+    }
+
+    # Check if auth.py set g.user
+    if g.user:
+        initial_state.update({
+            "points": g.user.points,
+            "current_level": g.user.current_level,
+            "current_type": g.user.current_type,
+            "clicks_remaining": g.user.clicks_remainiing,
+            "progress_percent": g.user.progress_percent,
+            "is_guest": False
+        })
+    
+    return render_template("game.html", state=initial_state)
--- Clicking_Game/static/js/game.js
+++ Clicking_Game/static/js/game.js
@@ -15,34 +15,56 @@ if (playBtns.length > 0) {
         water: {name: "Water Egg", baseClicks: 20, basePoints: 5} // example additonal type
     };
 
-    // Game State:
-    let totalPoints = 0;
-    let currentLevel = 1;
-    let currentType = 'standard';
-    let clicksRemaning = EGG_CONFIG[currentType].baseClicks;
+    // Game State (default/guest):
+    let gameState = {
+        totalPoints: INITIAL_STATE.points,
+        currentLevel: INITIAL_STATE.current_level,
+        currentType: INITIAL_STATE.current_type,
+        clicksRemaning: INITIAL_STATE.clicks_remaining ?? EGG_CONFIG[INITIAL_STATE.current_type].baseClicks,
+        isGuest: INITIAL_STATE.is_guest
+    };
+
+    
+
+    updateProgressUI();
 
     function handleEggClick() {
-        clicksRemaning--;
-        if (clicksRemaning <= 0) {
+        gameState.clicksRemaning--;
+        updateProgressUI()
+        if (gameState.clicksRemaning <= 0) {
             console.log("zero clicks left"); // no clicks left print
             const earned = calculateReward();
-            totalPoints += earned;
-            updateUI(earned);
+            gameState.totalPoints += earned;
+            updatePointsUI(earned);
         }
     }
 
     function calculateReward() { // add level as a parameter once added
         const level = 1 // placeholder for level (should probably be renamed to power to not be confused with levels as in stages)
 
-        const egg = EGG_CONFIG[currentType];
+        const egg = EGG_CONFIG[gameState.currentType];
         return Math.floor(egg.basePoints * level);
     }
 
-    function updateUI(pointsEarned) {
-        document.getElementById('egg-points').innerText = totalPoints;
+    function updatePointsUI(pointsEarned) {
+        document.getElementById('egg-points').innerText = gameState.totalPoints;
         console.log(`Earned ${pointsEarned} points!`); // amount rewarded print
+    }
+
+    function updateProgressUI() {
+        const requiredClicks = EGG_CONFIG[gameState.currentType].baseClicks
+        const percentage = ((requiredClicks - gameState.clicksRemaning) / requiredClicks) * 100;
+        const barWidth = Math.min(Math.max(percentage, 0), 100);
+
+        document.getElementById('progress-bar').style.width = `${barWidth}%`;
+        document.getElementById('click-count').innerText = `${gameState.clicksRemaning}/${requiredClicks} left`;
     }
 
     // Event Listeners/Triggers:
     document.getElementById('egg-btn').addEventListener('click', handleEggClick);
+
+    window.addEventListener('load', () => { // only displays game content on the html once all assets are loaded
+        document.getElementById('loading-overlay').style.display = 'none';
+        document.getElementById('game-screen').style.display ='';
+    })
 })();
