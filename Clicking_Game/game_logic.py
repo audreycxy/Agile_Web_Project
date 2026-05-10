40e98dbc65b2c9e1bca8a43477874f5e592012f3
James-909 <j.w.osmond@outlook.com>
2026-05-10 21:13:08 +0800

    Added: DB sync and a new GameState table. Fixed: current type being undefined after breaking last egg.

---
+++ Clicking_Game/game_logic.py
@@ -0,0 +1,21 @@
+# Egg Data:
+EGG_CONFIG = {
+    "standard": {
+        "name": "Standard",
+        "base_clicks": 10,
+        "base_points": 1,
+        "image": "/static/images/defaultegg_nobackground.png"
+    },
+    "water": {
+        "name": "Water",
+        "base_clicks": 20,
+        "base_points": 5,
+        "image": "/static/images/wateregg.png"
+    },
+    "gold": {
+        "name": "Golden",
+        "base_clicks": 30,
+        "base_points": 30,
+        "image": "/static/images/defaultegg_nobackground.png"
+    }
+}
